f72d4e50b95fc664b91246592c8b5713f44219eb
rensjutzua <47052730+rensjutzua@users.noreply.github.com>
2019-01-26 17:50:42 +0800

    Create Loops.c

---
+++ Loops.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int x=0;
+    while(x<100){
+        printf("%i\n",x);
+        x++;}
+    
+    int y=0;
+    do{
+        printf("%i\n",y);
+        y++;
+    }while(y<200);
+    
+    for(int z=0;z<100;z++){
+        printf("%i\n",z);
+
+    }
+    
+    return 0;
+}
