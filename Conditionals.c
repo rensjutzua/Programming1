bdfd0cbb9f02b345640d12c5404bac02a3c4e36e
rensjutzua <47052730+rensjutzua@users.noreply.github.com>
2019-01-26 16:29:01 +0800

    Create Conditionals.c
    Created conditionals

---
+++ Conditionals.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int Basketball = 0;
+    if(Basketball>=1000){
+        printf("Laban tayo");
+}
+    else if(Basketball>=600){
+        printf("Pwede na");
+}
+    else {
+        printf("Wag na");
+    }
+    
+
+    return 0;
+}
